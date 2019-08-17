bfa218513f44a45a98c18f9d8f32f62b4d7250e5
sheet00 <sheet00@rd5.so-net.ne.jp>
2019-08-17 13:18:11 +0900

    graph auto toggle

--- main.js
+++ main.js
@@ -3,11 +3,46 @@ var main = {};
 main.init = function () {
     main.reload(60);
     main.draw_chart();
+
+    $('.container :not(#news)').on('click', function () {
+        $('#graph').toggle();
+    })
+
+    main.auto_toggle(15);
 };
 
 main.reload = function (wait_second) {
     var second = wait_second * 1000
     setTimeout(function () { window.location.reload(); }, second);
+};
+
+main.auto_toggle = function (switch_second) {
+    var second = switch_second * 1000
+    //wait
+    main.loopSleep(1,switch_second,null);
+    //toggle
+    main.loopSleep(10, second, function (i) {
+        $('#graph').toggle();
+    })
+};
+
+main.loopSleep = function (_loopLimit, _interval, _mainFunc) {
+    var loopLimit = _loopLimit;
+    var interval = _interval;
+    var mainFunc = _mainFunc;
+    var i = 0;
+    var loopFunc = function () {
+        var result = mainFunc(i);
+        if (result === false) {
+            // break機能
+            return;
+        }
+        i = i + 1;
+        if (i < loopLimit) {
+            setTimeout(loopFunc, interval);
+        }
+    }
+    loopFunc();
 };
 
 main.draw_chart = function (data) {
