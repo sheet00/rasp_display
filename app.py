5192686b4946b602ecfb981bf49280eb5799458d
sheet00 <sheet00@rd5.so-net.ne.jp>
2025-03-16 15:40:05 +0900

    [change] flaskテンプレートエンジン設定

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-import os
-import random
-
-import time
-import board
-import adafruit_dht
-from datetime import datetime, timedelta
-from pytz import timezone
-from dateutil import parser
-
-import requests
-from bs4 import BeautifulSoup
-
-import pymysql.cursors
-import json
-from decimal import Decimal
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -22,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('template.html')
 
 
 if __name__ == '__main__':
